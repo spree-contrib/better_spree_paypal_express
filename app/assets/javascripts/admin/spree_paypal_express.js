fb5748aa7b83b5ad87d51b503c6c25c69e1044a0
Ryan Bigg <radarlistener@gmail.com>
2013-08-08 13:53:06 +1000

    Disable the use of the PayPal payment method in the admin backend

--- app/assets/javascripts/admin/spree_paypal_express.js
+++ app/assets/javascripts/admin/spree_paypal_express.js
@@ -1,1 +1,25 @@
 //= require admin/spree_backend
+
+SpreePaypalExpress = {
+  hideSettings: function(paymentMethod) {
+    if (SpreePaypalExpress.paymentMethodID && paymentMethod.val() == SpreePaypalExpress.paymentMethodID) {
+      $('.payment-method-settings').children().hide();
+      $('#payment_amount').attr('disabled', 'disabled');
+      $('button[type="submit"]').attr('disabled', 'disabled');
+      $('#paypal-warning').show();
+    } else {
+      $('.payment-method-settings').children().show();
+      $('button[type=submit]').attr('disabled', '');
+      $('#payment_amount').attr('disabled', '')
+      $('#paypal-warning').hide();
+    }
+  }
+}
+
+$(document).ready(function() {
+  checkedPaymentMethod = $('#payment-methods input[type="radio"]:checked');
+  SpreePaypalExpress.hideSettings(checkedPaymentMethod);
+  paymentMethods = $('#payment-methods input[type="radio"]').click(function (e) {
+    SpreePaypalExpress.hideSettings($(e.target));
+  });
+})
