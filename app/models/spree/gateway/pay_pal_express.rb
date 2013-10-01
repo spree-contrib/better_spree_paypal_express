93e9a7b6a161eca998c9f83a6a4102f7056e7eea
Ryan Bigg <radarlistener@gmail.com>
2013-10-02 09:11:32 +1000

    Convert amounts like '21,99' to '21.99' because the Big International Payment Company can't handle international amounts
    Fixes #11
    Fixes #30

--- app/models/spree/gateway/pay_pal_express.rb
+++ app/models/spree/gateway/pay_pal_express.rb
@@ -42,7 +42,14 @@ module Spree
           :PaymentDetails => [{
             :OrderTotal => {
               :currencyID => Spree::Config[:currency],
-              :value => ::Money.new(amount, Spree::Config[:currency]).to_s }
+              # gsub is here because PayPal fails to acknowledge 
+              # that some people like their currencies written as:
+              # 21,99
+              # As opposed to:
+              # 21.99
+              # The international payments company fails to handle
+              # international payment amounts. SMH.
+              :value => ::Money.new(amount, Spree::Config[:currency]).to_s.gsub(',', '.') }
           }]
         }
       })
