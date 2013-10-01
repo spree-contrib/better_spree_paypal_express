ff861ef5fbe2a58121eef1ca2c2173992624e19e
Mike Monaghan <mike@ignitedsoftware.com>
2013-10-02 00:12:42 +1000

    Removed :without_protection for Rails 4.0 compatibility

--- app/models/spree/gateway/pay_pal_express.rb
+++ app/models/spree/gateway/pay_pal_express.rb
@@ -82,7 +82,7 @@ module Spree
           :refund_transaction_id => refund_transaction_response.RefundTransactionID,
           :state => "refunded",
           :refund_type => refund_type
-        }, :without_protection => true)
+        } )
       end
       refund_transaction_response
     end
