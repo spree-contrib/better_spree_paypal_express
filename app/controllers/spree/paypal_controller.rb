d18f875bfdcdeb29ce708c2a47f3dc643094f7be
azini <aazadi@gmail.com>
2013-11-09 17:45:28 +0100

    - use variant sku instead of product sku
    [Fixes #36]

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -4,7 +4,7 @@ module Spree
       items = current_order.line_items.map do |item|
         {
           :Name => item.product.name,
-          :Number => item.product.sku,
+          :Number => item.variant.sku,
           :Quantity => item.quantity,
           :Amount => {
             :currencyID => current_order.currency,
