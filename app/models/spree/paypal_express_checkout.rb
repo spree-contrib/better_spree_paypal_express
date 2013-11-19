308ef961b4ba6ae83e354dabf61952535c0c5238
Stephane Bounmy <stephanebounmy@gmail.com>
2013-11-19 14:11:44 +0100

    fix: can_credit? condition

--- app/models/spree/paypal_express_checkout.rb
+++ app/models/spree/paypal_express_checkout.rb
@@ -8,8 +8,8 @@ module Spree
     end
 
     def can_credit?(payment)
-      return false unless payment.state == "completed"
-      return false unless payment.order.payment_state == "credit_owed"
+      return false unless payment.completed?
+      return false unless payment.order.outstanding_balance?
       return false unless payment.payment_method.payment_profiles_supported?
       payment.credit_allowed > 0
     end
