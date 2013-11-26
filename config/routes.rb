638761be47759676d26fc32f5cab9ee1cfdc29a5
Ryan Bigg <radarlistener@gmail.com>
2013-11-27 08:30:57 +1100

    Use Spree::Core::Engine.add_routes to draw the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.draw do
+Spree::Core::Engine.add_routes do
   post '/paypal', :to => "paypal#express", :as => :paypal_express
   get '/paypal/confirm', :to => "paypal#confirm", :as => :confirm_paypal
   get '/paypal/cancel', :to => "paypal#cancel", :as => :cancel_paypal
