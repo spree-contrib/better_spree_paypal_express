4d90b52fdb8cd10eb43a4717ec2b609763ecbd75
Ryan Bigg <radarlistener@gmail.com>
2013-11-27 08:50:54 +1100

    Fix pay_pal_express_spec model by stubbing more of the right things

--- spec/models/pay_pal_express_spec.rb
+++ spec/models/pay_pal_express_spec.rb
@@ -16,22 +16,35 @@ describe Spree::Gateway::PayPalExpress do
       provider
     end
 
-    # Test for #11
-    it "succeeds" do
+    before do
+      provider.should_receive(:build_get_express_checkout_details).with({
+        :Token => 'fake_token'
+      }).and_return(pp_details_request = double)
+
+      pp_details_response = double(:get_express_checkout_details_response_details =>
+        double(:PaymentDetails => {
+          :OrderTotal => {
+            :currencyID => "USD",
+            :value => "10.00"
+          }
+        }))
+
+      provider.should_receive(:get_express_checkout_details).
+        with(pp_details_request).
+        and_return(pp_details_response)
+
       provider.should_receive(:build_do_express_checkout_payment).with({
         :DoExpressCheckoutPaymentRequestDetails => {
           :PaymentAction => "Sale",
           :Token => "fake_token",
           :PayerID => "fake_payer_id",
-          :PaymentDetails => [
-            { :OrderTotal => {
-                :currencyID => "USD",
-                :value => "10.00"
-              }
-            }
-          ]
+          :PaymentDetails => pp_details_response.get_express_checkout_details_response_details.PaymentDetails
         }
       })
+    end
+
+    # Test for #11
+    it "succeeds" do
       response = double('pp_response', :success? => true)
       response.stub_chain("do_express_checkout_payment_response_details.payment_info.first.transaction_id").and_return '12345'
       provider.should_receive(:do_express_checkout_payment).and_return(response)
@@ -40,7 +53,6 @@ describe Spree::Gateway::PayPalExpress do
 
     # Test for #4
     it "fails" do
-      provider.should_receive(:build_do_express_checkout_payment)
       response = double('pp_response', :success? => false, 
                           :errors => [double('pp_response_error', :long_message => "An error goes here.")])
       provider.should_receive(:do_express_checkout_payment).and_return(response)
