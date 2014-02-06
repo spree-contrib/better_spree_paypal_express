32f319c19d5bf7ff4641948a5a64fafe521e1deb
Ryan Bigg <radarlistener@gmail.com>
2014-02-06 08:16:17 +0000

    Refer to correct assets in install_generator
    Fixes #70

--- lib/generators/spree_paypal_express/install/install_generator.rb
+++ lib/generators/spree_paypal_express/install/install_generator.rb
@@ -5,8 +5,8 @@ module SpreePaypalExpress
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_paypal_express\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_paypal_express\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/paypal_express\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/paypal_express\n"
       end
 
       def add_stylesheets
@@ -14,8 +14,8 @@ module SpreePaypalExpress
         backend_css_file = "vendor/assets/stylesheets/spree/backend/all.css"
 
         if File.exist?(backend_css_file) && File.exist?(frontend_css_file)
-          inject_into_file frontend_css_file, " *= require spree/frontend/spree_paypal_express\n", :before => /\*\//, :verbose => true
-          inject_into_file backend_css_file, " *= require spree/backend/spree_paypal_express\n", :before => /\*\//, :verbose => true
+          inject_into_file frontend_css_file, " *= require spree/frontend/paypal_express\n", :before => /\*\//, :verbose => true
+          inject_into_file backend_css_file, " *= require spree/backend/paypal_express\n", :before => /\*\//, :verbose => true
         end
       end
 
