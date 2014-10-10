e81fddf2818db9c890381408aedb1827c44bd6e9
Frederic Jean <fred@fredjean.net>
2014-10-10 14:03:02 -0600

    Further musing on where things belong...

--- spec/features/paypal_spec.rb
+++ spec/features/paypal_spec.rb
@@ -18,6 +18,36 @@ describe "PayPal", :js => true do
     FactoryGirl.create(:shipping_method)
   end
 
+  def fill_in_billing
+    within("#billing") do
+      fill_in "First Name", :with => "Test"
+      fill_in "Last Name", :with => "User"
+      fill_in "Street Address", :with => "1 User Lane"
+      # City, State and ZIP must all match for PayPal to be happy
+      fill_in "City", :with => "Adamsville"
+      select "United States of America", :from => "order_bill_address_attributes_country_id"
+      select "Alabama", :from => "order_bill_address_attributes_state_id"
+      fill_in "Zip", :with => "35005"
+      fill_in "Phone", :with => "555-123-4567"
+    end
+  end
+
+  def fill_in_shipping
+    uncheck("order[use_billing]")
+    within("#shipping") do
+      fill_in "First Name", :with => "Test"
+      fill_in "Last Name", :with => "User"
+      fill_in "Street Address", :with => "2 User Lane"
+      # City, State and ZIP must all match for PayPal to be happy
+      fill_in "City", :with => "Adamsville"
+      select "United States of America", :from => "order_ship_address_attributes_country_id"
+      select "Alabama", :from => "order_ship_address_attributes_state_id"
+      fill_in "Zip", :with => "35005"
+      fill_in "Phone", :with => "555-123-4567"
+    end
+  end
+
+
   def add_product_to_cart(product)
     visit spree.root_path
     click_link product
--- spec/support/paypal_support.rb
+++ spec/support/paypal_support.rb
@@ -20,35 +20,6 @@ module PaypalSupport
     end
   end
 
-  def fill_in_billing
-    within("#billing") do
-      fill_in "First Name", :with => "Test"
-      fill_in "Last Name", :with => "User"
-      fill_in "Street Address", :with => "1 User Lane"
-      # City, State and ZIP must all match for PayPal to be happy
-      fill_in "City", :with => "Adamsville"
-      select "United States of America", :from => "order_bill_address_attributes_country_id"
-      select "Alabama", :from => "order_bill_address_attributes_state_id"
-      fill_in "Zip", :with => "35005"
-      fill_in "Phone", :with => "555-123-4567"
-    end
-  end
-
-  def fill_in_shipping
-    uncheck("order[use_billing]")
-    within("#shipping") do
-      fill_in "First Name", :with => "Test"
-      fill_in "Last Name", :with => "User"
-      fill_in "Street Address", :with => "2 User Lane"
-      # City, State and ZIP must all match for PayPal to be happy
-      fill_in "City", :with => "Adamsville"
-      select "United States of America", :from => "order_ship_address_attributes_country_id"
-      select "Alabama", :from => "order_ship_address_attributes_state_id"
-      fill_in "Zip", :with => "35005"
-      fill_in "Phone", :with => "555-123-4567"
-    end
-  end
-
   def go_to_paypal
     find("#paypal_button").click
 
@@ -57,13 +28,11 @@ module PaypalSupport
     #
     # This changes the behavior of the specs to match the layout that Paypal
     # returns to us.
-    # unless respond_to?(:login_to_paypal)
-      if paypal_layout == :new_layout
-        self.class.include NewPaypal
-      else
-        self.class.include OldPaypal
-      end
-    # end
+    if paypal_layout == :new_layout
+      extend NewPaypal
+    else
+      extend OldPaypal
+    end
   end
 
   module OldPaypal
@@ -93,6 +62,8 @@ module PaypalSupport
 
   module NewPaypal
     def login_to_paypal
+      find("#loginForm")
+
       within("#loginForm") do
         fill_in "Email", :with => "pp@spreecommerce.com"
         fill_in "Password", :with => "thequickbrownfox"
