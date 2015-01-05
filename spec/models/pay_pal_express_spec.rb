e4ceffd5951c575b8f22924685f5ab5dde0896e7
Tobias Bohwalli <hi@futhr.io>
2015-01-05 12:15:53 +0100

    Don't require spec_helper twice.
    This already set in .rspec config file.

--- spec/models/pay_pal_express_spec.rb
+++ spec/models/pay_pal_express_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe Spree::Gateway::PayPalExpress do
   let(:gateway) { Spree::Gateway::PayPalExpress.create!(name: "PayPalExpress", :environment => Rails.env) }
 
@@ -53,7 +51,7 @@ describe Spree::Gateway::PayPalExpress do
 
     # Test for #4
     it "fails" do
-      response = double('pp_response', :success? => false, 
+      response = double('pp_response', :success? => false,
                           :errors => [double('pp_response_error', :long_message => "An error goes here.")])
       provider.should_receive(:do_express_checkout_payment).and_return(response)
       lambda { payment.purchase! }.should raise_error(Spree::Core::GatewayError, "An error goes here.")
