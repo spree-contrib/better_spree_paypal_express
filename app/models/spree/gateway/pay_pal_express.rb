90278e0d600b97eb697d4fec58bbd648baf5eb2c
Sudeep Sidhu <sidhu.sudeep@gmail.com>
2015-10-12 03:06:38 -0500

    Void and Cancel methods added. They use a "refund" to accomplish their behaviour. Works for partially refunded payments as well.

--- app/models/spree/gateway/pay_pal_express.rb
+++ app/models/spree/gateway/pay_pal_express.rb
@@ -33,6 +33,62 @@ module Spree
     def method_type
       'paypal'
     end
+    
+    # This is rather hackish, required for payment/processing handle_response code.
+    def empty_success
+      Class.new do
+        def success?; true; end
+        def authorization; nil; end
+      end.new
+    end
+
+    #this is essentially the refund method below except the creation of the refund transaction
+    #as that will count doubly towards the "credited" amount
+    def void(response_code, gateway_options={})
+      payment = Spree::Payment.find_by_response_code(response_code)
+      amount = payment.credit_allowed
+
+      #in case a partially refunded payment gets cancelled/voided, we don't want to act on the refunded payments
+      if amount.to_f > 0
+
+        # Process the refund
+        refund_type = payment.amount == amount.to_f ? "Full" : "Partial"
+
+        refund_transaction = provider.build_refund_transaction({
+          :TransactionID => payment.source.transaction_id,
+          :RefundType => refund_type,
+          :Amount => {
+            :currencyID => payment.currency,
+            :value => amount },
+          :RefundSource => "any" })
+        
+        refund_transaction_response = provider.refund_transaction(refund_transaction)
+      
+        if refund_transaction_response.success?
+          payment.source.update_attributes({
+            :refunded_at => Time.now,
+            :refund_transaction_id => refund_transaction_response.RefundTransactionID,
+            :state => "refunded",
+            :refund_type => refund_type
+          })
+          empty_success
+        else
+          class << refund_transaction_response
+            def to_s
+              errors.map(&:long_message).join(" ")
+            end
+          end
+          refund_transaction_response
+        end
+      end
+
+      empty_success
+    end
+
+    #cancellations also work for a partially refunded payment
+    def cancel(response_code)
+      void(response_code, {})
+    end
 
     def purchase(amount, express_checkout, gateway_options={})
       pp_details_request = provider.build_get_express_checkout_details({
@@ -55,11 +111,12 @@ module Spree
         # This is mainly so we can use it later on to refund the payment if the user wishes.
         transaction_id = pp_response.do_express_checkout_payment_response_details.payment_info.first.transaction_id
         express_checkout.update_column(:transaction_id, transaction_id)
-        # This is rather hackish, required for payment/processing handle_response code.
-        Class.new do
-          def success?; true; end
-          def authorization; nil; end
-        end.new
+
+        #"order_id: The Order’s number attribute, plus the identifier for each payment, generated when the payment is first created"
+        payment = Spree::Payment.find_by_number(gateway_options[:order_id].split('-').last)
+        payment.update_attribute(:response_code, transaction_id)
+        
+        empty_success
       else
         class << pp_response
           def to_s
