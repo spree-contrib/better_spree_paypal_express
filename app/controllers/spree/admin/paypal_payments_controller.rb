7b3f0530cc947dd576b471e0db0a825ea258897e
Man Vuong <thanhman.gm@gmail.com>
2017-08-14 03:58:43 +0700

    Replace before_filter with before_action

--- app/controllers/spree/admin/paypal_payments_controller.rb
+++ app/controllers/spree/admin/paypal_payments_controller.rb
@@ -1,6 +1,6 @@
 module Spree
   class Admin::PaypalPaymentsController < Spree::Admin::BaseController
-    before_filter :load_order
+    before_action :load_order
 
     def index
       @payments = @order.payments.includes(:payment_method).where(:spree_payment_methods => { :type => "Spree::Gateway::PayPalExpress" })
