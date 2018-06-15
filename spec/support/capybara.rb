ef31ca82ffbc30b85dc685be92e944d76f6ce8ca
Fernando Valverde Arredondo <fdov88@gmail.com>
2018-06-15 10:51:15 -0600

    travis.yml revamp and passing tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,6 +7,6 @@ RSpec.configure do
   Capybara.javascript_driver = :poltergeist
 
   Capybara.register_driver(:poltergeist) do |app|
-    Capybara::Poltergeist::Driver.new app, js_errors: true, timeout: 60, phantomjs_options: ['--ssl-protocol=TLSv1.2']
+    Capybara::Poltergeist::Driver.new app, js_errors: true, timeout: 180, phantomjs_options: ['--ssl-protocol=TLSv1.2']
   end
 end
