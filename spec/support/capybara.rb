bcdb220f9ca87f66dcd61c43e11ed9bee7b518fa
Matthew Kennedy <m.kennedy@me.com>
2020-09-03 00:08:28 +0100

    use new spree_dev_tools

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,13 +4,13 @@ require 'capybara/rails'
 require 'selenium-webdriver'
 require 'webdrivers'
 
-RSpec.configure do |config|
+RSpec.configure do |_config|
   Capybara.register_driver :chrome do |app|
     Selenium::WebDriver.logger.level = :error
 
     Capybara::Selenium::Driver.new app,
-      browser: :chrome,
-      options: Selenium::WebDriver::Chrome::Options.new(args: %w[disable-popup-blocking headless disable-gpu window-size=1920,1080])
+                                   browser: :chrome,
+                                   options: Selenium::WebDriver::Chrome::Options.new(args: %w[disable-popup-blocking headless disable-gpu window-size=1920,1080])
   end
 
   Capybara.javascript_driver = :chrome
--- spec/support/capybara_ext.rb
+++ spec/support/capybara_ext.rb
@@ -1,7 +1,7 @@
 module CapybaraExt
   def wait_for(options = {})
     default_options = { error: nil, seconds: 5 }.merge(options)
-  
+
     Selenium::WebDriver::Wait.new(timeout: default_options[:seconds]).until { yield }
   rescue Selenium::WebDriver::Error::TimeOutError
     default_options[:error].nil? ? false : raise(default_options[:error])
